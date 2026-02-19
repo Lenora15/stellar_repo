12ff4470cf558d62e3dec5b833f0386ab0d22f74
Lenora15 <lenora.ramirezwildman@outlook.com>
2026-02-19 02:57:03 -0500

    oops forgot to save!

--- stellar_project/lib/main.dart
+++ stellar_project/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'Pages/home.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'Pages/authentication/login.dart';
 import 'themes/app_themes.dart';
 
