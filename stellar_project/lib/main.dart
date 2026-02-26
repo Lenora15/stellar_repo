8eae464dd23d5bf2af83572285d66db29d790eb6
Lenora15 <lenora.ramirezwildman@outlook.com>
2026-02-25 20:23:42 -0500

    additions to authenticaiton

--- stellar_project/lib/main.dart
+++ stellar_project/lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:stellar_project/Pages/home.dart';
+import 'package:stellar_project/Pages/main_screen.dart';
 import 'firebase_options.dart';
 import 'Pages/authentication/login.dart';
 import 'themes/app_themes.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -33,7 +38,24 @@ class MyApp extends StatelessWidget {
           child: child,
         );
       },
-      home: const LoginScreen(),
+
+      // handles login state. If user is logged in, home page will display.
+      // if not they have to log in or create an account.
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot){
+          if (snapshot.connectionState == ConnectionState.waiting){
+            return const Scaffold(
+              body: Center(child: CircularProgressIndicator()),
+            );
+          }
+          //takes the user to the home page upon opening/login
+          if (snapshot.hasData){
+            return const MainScreen();
+          }
+          return const LoginScreen();
+        }
+      ),
     );
   }
 }
